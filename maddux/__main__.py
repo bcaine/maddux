7708e3e44b21af640e91ba2be48ca953352806e0
Ben Caine <bcaine0@gmail.com>
2016-04-03 21:00:36 -0400

    Added main and cli

---
+++ maddux/__main__.py
@@ -0,0 +1,49 @@
+"""
+Maddux is an experiment with Reinforcement Learning
+on Robot Arms, teaching them to throw.
+
+Written from scratch because we're masochists.
+"""
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Reinforcement Learning for robot arm throwing")
+    parser.add_argument('-t', '--train', action='store_true',
+                        help='Train the robot')
+    parser.add_argument('-e', '--evaluate', action='store_true',
+        help='Evaluate the arm by throwing once.')
+    parser.add_argument('-p', '--path', type=str, required=True,
+                        help='Path to weight file')
+
+    args = parser.parse_args()
+
+    if args.train:
+        train(args.path)
+    elif args.evaluate:
+        evaluate(args.path)
+    else:
+        parser.print_help()
+
+
+def train(path):
+    """
+    Train the robot arm with RL to throw a ball
+
+    :param path: Path to save location of training data
+    """
+    pass
+
+
+def evaluate(path):
+    """
+    Evaluate the robot arm throwing with RL
+    
+    :param path: Path to save location of the training data
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
