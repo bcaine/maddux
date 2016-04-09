5cd1689ed4b923b77efe0655e69f28fb6f0c52a3
bcaine <bcaine0@gmail.com>
2016-04-09 17:03:52 -0400

    Adding plotting to environment

--- maddux/environment.py
+++ maddux/environment.py
@@ -2,6 +2,9 @@
 Our experiment environment.
 """
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 from objects import Ball, Target
 
 class Environment:
@@ -50,3 +53,24 @@ class Environment:
         return False
             
                     
+    def plot(self):
+        """Plot throw trajectory and ball"""
+        positions = np.array(self.ball.positions)
+
+        fig = plt.figure(figsize=(12, 8))
+        ax = fig.gca(projection='3d')
+        ax.plot(positions[:, 0], positions[:, 1], positions[:, 2], label='Trajectory')
+
+        u = np.linspace(0, 2 * np.pi, 100)
+        v = np.linspace(0, np.pi, 100)
+
+        x = 2 * self.ball.radius * np.outer(np.cos(u), np.sin(v)) + self.ball.position[0]
+        y = 2 * self.ball.radius * np.outer(np.sin(u), np.sin(v)) + self.ball.position[1]
+        z = 2 * self.ball.radius * np.outer(np.ones(np.size(u)), np.cos(v)) + self.ball.position[2]
+        ax.plot_surface(x, y, z, rstride=4, cstride=4, color='b')
+
+        ax.set_xlim([0, 10])
+        ax.set_ylim([0, 10])
+        ax.set_zlim([0, 10])
+        plt.show()
+        
