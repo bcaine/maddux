62c42c404a71c8cd1f2389362cf9e3d34880641f
bcaine <bcaine0@gmail.com>
2016-04-09 15:35:16 -0400

    Small clean ups

--- maddux/environment.py
+++ maddux/environment.py
@@ -6,38 +6,26 @@ from objects import Ball, Target
 
 class Environment:
 
-    def __init__(self, dimensions=None):
+    def __init__(self, ball, target, robot=None, dimensions=None):
         """
         Our room goes from [0, 0, 0] to [x, y, z]
         :param dimensions: 1x3 array of the dimensions of the room.
         """
         self.dimensions = dimensions if dimensions else np.array([10.0, 10.0, 100.0])
-        self.objects = {}
+        self.ball = ball
+        self.target = target
+        self.robot = robot
         
-
-    def add_object(self, obj):
-        """
-        :param obj: An object (like a ball, robot, or target) to add
-                    to the environment.
-        """
-        self.objects[obj.name] = obj
-
 
     def run(self, duration):
         """Run for a certain duration
         
         :param duration: duration to run environment in ms
-
         :return score: The value showing how close the ball is to the target
         """
-        assert "ball" in self.objects
-        assert "target" in self.objects
-
-        ball = self.objects["ball"]
-        target = self.objects["target"]
 
         for _ in xrange(duration):
-            ball.step()
+            self.ball.step()
             if self._is_collision(ball, target):
                 break
         return target.get_score(ball.leading_point())
@@ -48,15 +36,15 @@ class Environment:
         
         :param ball: Our ball object
         :param target: Our target object
+        
+        :return: Boolean, whether there was a collision
         """
         if target.is_hit(ball):
             ball.attach()
             return True
 
         for i, obj_dim in obj.position:
-            # If in a negative dimension, or it hit a wall
             if obj_dim <= 0 or obj_dim >= self.dimensions[i]:
-                # Attach it to that surface
                 obj.attach()
                 return True
 
