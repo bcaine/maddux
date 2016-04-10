660d4737d1b710b929950822764f080366f31423
Ben Caine <bcaine0@gmail.com>
2016-04-10 19:29:53 -0400

    Fixed typo in robot utils

--- maddux/robots/utils.py
+++ maddux/robots/utils.py
@@ -4,7 +4,7 @@ Random collection of utilities for the robots to use
 
 import numpy as np
 
-def get_rotation_from_homogenous_transfrom(transform):
+def get_rotation_from_homogenous_transform(transform):
     s = transform.shape
     if s[0] != s[1]:
       raise ValueError('Matrix must be a 4x4 homogenous transformation', s)
