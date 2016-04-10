6332ea2f0ad9c94e629611e318967895ef2f3a01
Ben Caine <bcaine0@gmail.com>
2016-04-10 17:54:42 -0400

    Restructuring plotting

--- maddux/objects/ball.py
+++ maddux/objects/ball.py
@@ -17,3 +17,21 @@ class Ball(ThrowableObject):
            that will hit the target.
         """
         return self.position + np.array([0, self.radius, 0])
+
+
+    def plot(self, ax):
+        """Plots the ball at its current location.
+
+        :param ax: Figure to plot on.
+        """
+        u = np.linspace(0, 2 * np.pi, 100)
+        v = np.linspace(0, np.pi, 100)
+
+        x = (2 * self.radius * np.outer(np.cos(u), np.sin(v)) +
+              self.position[0])
+        y = (2 * self.radius * np.outer(np.sin(u), np.sin(v)) +
+              self.position[1])
+        z = (2 * self.radius * np.outer(np.ones(np.size(u)), np.cos(v)) +
+              self.position[2])
+        ax.plot_surface(x, y, z, rstride=4, cstride=4, color='g')
+        
