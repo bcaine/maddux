aa9d87fb58ae15a4a53aa610c99c7ae822f45a73
bcaine <bcaine0@gmail.com>
2016-04-11 16:58:59 -0400

    Cleaned up test __init__

--- maddux/tests/__init__.py
+++ maddux/tests/__init__.py
@@ -2,8 +2,8 @@ from arm_test import arm_test
 from animation_test import animation_test
 
 tests = {
-    'arm_test': arm_test,
-    'animation_test': animation_test
+    'arm': arm_test,
+    'animation': animation_test
 }
 
 def run_test(test):
