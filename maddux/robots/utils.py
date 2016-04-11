3d72de893be8f13b5574adf0bf737868d1f5e237
ColinKohler <colink78@gmail.com>
2016-04-11 11:53:54 -0400

    Added documentation to utils that I frogot to add

--- maddux/robots/utils.py
+++ maddux/robots/utils.py
@@ -5,6 +5,10 @@ Random collection of utilities for the robots to use
 import numpy as np
 
 def get_rotation_from_homogenous_transform(transform):
+    """
+    Extract the rotation section of the homogenous transformation
+    :param transform: The homogenous transform to extract the rotation matrix from
+    """
     s = transform.shape
     if s[0] != s[1]:
       raise ValueError('Matrix must be a 4x4 homogenous transformation', s)
