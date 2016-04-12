1ea0609861fe2bfa2d62bca6c44abd70d8793249
Ben Caine <bcaine0@gmail.com>
2016-04-12 01:43:26 -0400

    Changed tests/ to be examples

--- maddux/__main__.py
+++ maddux/__main__.py
@@ -9,20 +9,40 @@ Rethink this when we get a bit further along
 """
 
 import argparse
-from tests import run_test, tests
+import warnings
+import sys
+from examples import run_example, examples
+
+
+class HelpfulParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        print "Examples: {}".format(examples.keys())
+        sys.exit(2)
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Reinforcement Learning for robot arm throwing")
-    parser.add_argument('-t', '--test', type=str, required=True,
-                        help="Which test to run.")
+    parser = HelpfulParser(description="A robotic arm toolbox")
+    parser.add_argument('-e', '--example', type=str, required=False,
+                        help="Which example to run.")
+    parser.add_argument('-t', '--test', type=str, required=False,
+                        help="DEPRECATED. Run a specific test")
 
-    args = parser.parse_args()
+    success = False
+    if len(sys.argv):
+        args = parser.parse_args()
+        if args.test:
+            warnings.warn("Please use -e/--example instead",
+                          DeprecationWarning)
+            success = run_example(args.test)
+        elif args.example:
+            success = run_example(args.example)
 
-    if not run_test(args.test):
+    if not success:
         parser.print_help()
-        print "Test Options: {}".format(tests.keys())
+        print "Test Options: {}".format(examples.keys())
+
 
 if __name__ == "__main__":
     main()
--- maddux/examples/__init__.py
+++ maddux/examples/__init__.py
@@ -3,7 +3,7 @@ from animation_test import ball_animation_test, arm_animation_test
 from plot_arm_test import plot_arm_test
 from plot_test import plot_test
 
-tests = {
+examples = {
     'arm': arm_test,
     'ball_animation': ball_animation_test,
     'arm_animation': arm_animation_test,
@@ -11,9 +11,9 @@ tests = {
     'plot': plot_test,
 }
 
-def run_test(test):
-    if test in tests:
-        tests[test]()
+def run_example(example):
+    if example in examples:
+        examples[example]()
         return True
     return False
     
