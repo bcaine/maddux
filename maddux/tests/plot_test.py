35caa7e05c7ed7ce87c348cb4bbcf801c00066e1
Ben Caine <bcaine0@gmail.com>
2016-04-11 23:38:55 -0400

    Adding plot test

---
+++ maddux/tests/plot_test.py
@@ -0,0 +1,15 @@
+import numpy as np
+from environment import Environment
+from objects import Ball, Target
+from robots import simple_human_arm
+
+def plot_test():
+    q0 = np.array([0.5, 0.2, 0, 0.5, 1.5])
+    human_arm = simple_human_arm(2.0, 2.0, q0, np.array([2.0, 2.0, 0.0]))
+    
+    ball = Ball(np.array([2.0, 0.0, 2.0]), 0.15)
+    target = Target(np.array([5.0, 18.0, 2.0]), 0.5)
+    env = Environment(ball, target, robot=human_arm,
+                      dimensions=[10.0, 20.0, 100.0])
+    
+    env.plot()
