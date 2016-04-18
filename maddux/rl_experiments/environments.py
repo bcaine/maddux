ba1d108e3888a0788c776b9c09890904fd9174b6
Ben Caine <bcaine0@gmail.com>
2016-04-18 00:33:23 -0400

    Renaming simple to easy

--- maddux/rl_experiments/environments.py
+++ maddux/rl_experiments/environments.py
@@ -9,25 +9,25 @@ import numpy as np
 
 room_dimensions = np.array([10.0, 10.0, 10.0])
 
-def get_simple_environment():
-    simple_obstacles = [Obstacle([1, 2, 1], [2, 2.5, 1.5]),
+def get_easy_environment():
+    easy_obstacles = [Obstacle([1, 2, 1], [2, 2.5, 1.5]),
                         Obstacle([3, 2, 1], [4, 2.5, 1.5])]
-    simple_ball = Ball([2.5, 2.5, 2.0], 0.25)
-    simple_q = np.array([0, 0, 0, np.pi / 2, 0, 0, 0])
-    simple_robot = simple_human_arm(2.0, 2.0, simple_q,
+    easy_ball = Ball([2.5, 2.5, 2.0], 0.25)
+    easy_q = np.array([0, 0, 0, np.pi / 2, 0, 0, 0])
+    easy_robot = easy_human_arm(2.0, 2.0, easy_q,
                                     np.array([3.0, 1.0, 0.0]))
 
     return Environment(room_dimensions,
-                       dynamic_objects=[simple_ball],
-                       static_objects=simple_obstacles,
-                       robot=simple_robot)
+                       dynamic_objects=[easy_ball],
+                       static_objects=easy_obstacles,
+                       robot=easy_robot)
 
 def get_medium_environment():
     medium_obstacles = [Obstacle([2.5, 0, 2.2], [3.5, 1, 2.5]),
                         Obstacle([3, 2, 1], [4, 2.5, 1.5])]
     medium_ball = Ball([2.5, 2.5, 2.0], 0.25)
     medium_q = np.array([0, 0, 0, 0, 0, 0, 0])
-    medium_robot = simple_human_arm(2.0, 2.0, medium_q,
+    medium_robot = easy_human_arm(2.0, 2.0, medium_q,
                                     np.array([3.0, 1.0, 0.0]))
 
     return Environment(room_dimensions,
@@ -43,7 +43,7 @@ def get_hard_environment():
                       Obstacle([0.5, 2.5, 0.0], [1.5, 4.0, 1.0])]
     hard_ball = Ball([1.0, 3.25, 2.0], 0.5)
     hard_q = np.array([0, 0, 0, -np.pi/2.0, 0, 0, 0])
-    hard_robot = simple_human_arm(3.0, 2.0, hard_q, np.array([1.0, 1.0, 0.0]))
+    hard_robot = easy_human_arm(3.0, 2.0, hard_q, np.array([1.0, 1.0, 0.0]))
 
     return Environment(room_dimensions,
                        dynamic_objects=[hard_ball],
@@ -51,7 +51,7 @@ def get_hard_environment():
                        robot=hard_robot)
 
 environments = {
-    "simple": get_simple_environment,
+    "easy": get_easy_environment,
     "medium": get_medium_environment,
     "hard": get_hard_environment,
 }
