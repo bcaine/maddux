7b4797bcbaecb0bb25459e5647caa7eeb048f656
bcaine <bcaine0@gmail.com>
2016-04-22 16:27:31 -0400

    Updated predefined robots comments

--- maddux/robots/predefined_robots.py
+++ maddux/robots/predefined_robots.py
@@ -4,17 +4,22 @@ import numpy as np
 
 
 def simple_human_arm(seg1_len, seg2_len, q0, base=None):
-    """
-    Creates a simple human-like robotic arm with 7 links and 2 segments
+    """Creates a simple human-like robotic arm with 7 links and 2 segments
     with the desired lengths and starting joint configuration
+
     :param seg1_len: The length of the first segment of the arm
-    :type seg1_len: integer
+    :type seg1_len: int
+
     :param seg2_len: The length of the second segment of the arm
-    :type seg2_len: integer
-    :param q0: The starting joint configuration
-    :type q0: 1xN numpy.array
-    :param base: (Optional) optional base location of arm
-    :type base: 1x3 numpy.array or None
+    :type seg2_len: int
+
+    :param q0: 1xN vector of the starting joint configuration
+    :type q0: numpy.ndarray
+
+    :param base: (Optional) (x, y, z) location of arm base
+    :type base: numpy.ndarray or None
+
+    :returns: 7 link, 2 segment "human" arm.
     :rtype: maddux.robot.Arm
     """
     L1 = Link(0, 0, 0, 1.571)
@@ -30,14 +35,18 @@ def simple_human_arm(seg1_len, seg2_len, q0, base=None):
     return robot
 
 def noodle_arm(seg_lens, q0, base=None):
-    """
-    Creates a complex arm with 10 segments
-    :param seg_lens: The lengths of each sement
-    :type seg_lens: 1x10 nump.array
-    :param q0: The starting joint configuration
-    :type 10: 1xN numpy.array
-    :param base: (Optional) optional base location of arm
-    :type base: 1x3 nump.array or None
+    """Creates a complex arm with 10 segments
+
+    :param seg_lens: 1x10 vector of lengths of each sement
+    :type seg_lens: numpy.ndarray
+
+    :param q0: 1xN vector of the starting joint configuration
+    :type q0: numpy.ndarray
+
+    :param base: (Optional) optional (x, y, z) base location of arm
+    :type base: numpy.ndarray or None
+
+    :returns: "Noodle" arm
     :rtype: maddux.robot.Arm
     """
     L1  = Link(0, seg_lens[0], 0, 1.571)
